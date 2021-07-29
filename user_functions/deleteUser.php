a4c0ac86cfa6f312d5cf2e0a1c583f58956108c1
germamzva <ranfeche@gmail.com>
2021-07-29 19:45:59 +0800

    add update when the user will deleted

--- user_functions/deleteUser.php
+++ user_functions/deleteUser.php
@@ -8,6 +8,10 @@ $data = array();
 
 $id = $_POST['id'];
 
+// update the table data where equal to user id
+$conn->query("UPDATE `list_tbl` SET `user_assigned_id` = '0', `user_assigned_name` = '' WHERE user_assigned_id = '".$id."'");
+
+// delete the user by id
 $delete = "DELETE FROM `user_list` WHERE `user_id` = $id";
 
 if($conn->query($delete) === TRUE){
